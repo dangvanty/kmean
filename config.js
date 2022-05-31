ea64dc9a61fedb2e2aae2bc92af85114a9c1b91d
Dang Van Ty <dangvantydh1@gmail.com>
2022-05-31 17:36:02 +0700

    complete demo

--- config.js
+++ config.js
@@ -1,10 +1,82 @@
-function chartData(){
+function ChartData(){
+    let clusterCenters=[
+        {x:33.125,y:23.875},
+        {x:39.375,y:28.25},
+        {x:34.25,y:27},
+        
+    ]
+        // for(let i=0;i<k;i++){
+        //     while(true){
+        //         let a= Number((Math.random()*100).toFixed(2));
+        //         let b=Number((Math.random()*100).toFixed(2))
+        //         if(a>=20 && a<60 && b>=10 && b<40){
+        //             clusterCenters.push({x:a,y:b})
+        //             break;
+        //         }
+                
+        //     }
+        // }
+    
+    const colors=['red', 'blue','green','yellow','purple','hotpink','black','orange','brown','grey']
     return{
         datasets:[{
             label:"Employee data chart",
-            data: dataSet().map((dataPoint)=>{
-                
-            })
+            data: clusterCenters.concat(dataSet().map((dataPoint)=>{return {
+                x:dataPoint.Age, y:dataPoint.Body_mass_index
+            }                
+            })),
+            pointStyle: clusterCenters.map(clusterCenter => 'triangle').concat(dataSet().map(dataPoint => "circle")),
+            pointRadius: clusterCenters.map(clusterCenter =>10).concat(dataSet().map(dataPoint => 5.5)),
+            pointBackgroundColor: colors.slice(0,k) ,
+            showLine:false,
+            backgroundColor:"aqua"
         }]
+    }
+}
+
+function chartOptions(){
+    return {
+        maintainAspectRatio: false,
+        legend:{
+            labels:
+            {
+                fontSize:20
+            }
+        },
+        responsive:true,
+        scales:{
+            xAxes:[
+                {
+                    display:true,
+                    scaleLabel:{
+                        display:true,
+                        labelString: "Age level",
+                        fontSize:20
+                    },
+                    ticks:{
+                        fontSize:20,
+                        max:60,
+                        min:20
+                
+                    }
+                }
+            ],
+            yAxes:[
+                {
+                    display:true,
+                    scaleLabel:{
+                        display:true,
+                        labelString: "Body_mass_index level",
+                        fontSize:20
+                    },
+                    ticks:{
+                        fontSize:20,
+                        max:40,
+                        min:10
+                
+                    }
+                }
+            ]
+        }
     }
 }
--- script.js
+++ script.js
@@ -1,5 +1,113 @@
-const k = Number(prompt("Chọn giá trị của K"));
+const k = 3;
+alert("chọn giá trị mặc định của K =",k)
 const chart =new Chart(document.querySelector("#chart"),{
     type:"scatter",
-    data:ChartData()
+    data:ChartData(),
+    options:chartOptions()
 }) 
+
+let preventCalllBack=false
+
+
+document.addEventListener('keydown', async()=>{
+if(!preventCalllBack){
+        preventCalllBack=true
+        
+    let clusterCentersLocs=['nhom'];
+    let clusterCentersNewLocs=['tam'];
+    while(JSON.stringify(clusterCentersLocs)!=JSON.stringify(clusterCentersNewLocs)){
+        clusterCentersLocs=[]
+        for(let i=0;i<k;i++){
+            clusterCentersLocs.push(chart.data.datasets[0].data[i])
+        }
+        await labelDataPoints()
+        await recenterClusterCenters()
+
+        clusterCentersNewLocs=[]
+        for(let i=0;i<k;i++){
+            clusterCentersNewLocs.push(chart.data.datasets[0].data[i])
+        }
+    }
+    alert('Hoàn thành thuật toán! Mở console để thấy kết quả')
+    consoleResults()
+}
+})
+
+function consoleResults(){
+    const cluster =getClusters()
+    for(let i=0; i<cluster.length;i++){
+        console.log(`nhóm ${String.fromCharCode(i+65)} nhân viên:`)
+        console.table(cluster[i])
+    }
+}
+
+function recenterClusterCenters(){
+    return new Promise(async(resolve,reject)=>{
+        const clusters=getClusters()
+        console.log( typeof clusters)
+        // for (let i=0; i<clusters.length;i++){
+        //     let sumX = 0
+        //     let sumY = 0
+        //     for(let j=i;j<clusters.length;j++){
+        //         sumX= sumX + clusters[j].x
+        //         sumY= sumY + clusters[j].y
+        //     }
+        //     if(clusters.length){
+        //         chart.data.datasets[0].data[i]={x:Number((sumX/clusters.length).toFixed(2)),y:Number((sumY/clusters.length).toFixed(2))}
+        //     }
+
+        // }
+    clusters.forEach((cluster,i)=>{
+        let sumX = 0
+        let sumY = 0
+        cluster.forEach((dataPoint,j)=>{
+            sumX= sumX + dataPoint.x
+            sumY= sumY + dataPoint.y
+        })
+        if(cluster.length){
+            chart.data.datasets[0].data[i]={x:Number((sumX/cluster.length).toFixed(2)),y:Number((sumY/cluster.length).toFixed(2))}
+        }
+      
+    })
+    // console.log(clusters)
+    chart.update(3000)
+    await new Promise(resolve =>setTimeout(resolve,3000))
+    resolve()
+    })
+}
+
+function getClusters(){
+    let clusters=[]
+    for(let i=0;i<k;i++){
+        clusters.push([])
+    }
+    const colors=['red', 'blue','green','yellow','purple','hotpink','black','orange','brown','grey'].slice(0,k)
+
+    for(let i=k; i<chart.data.datasets[0].data.length;i++){
+        for(let j=0; j<colors.length;j++){
+            if(chart.data.datasets[0].pointBackgroundColor[i]==colors[j]){
+                clusters[j].push(chart.data.datasets[0].data[i])
+            }
+        }
+    }
+    return clusters
+}
+function labelDataPoints(){
+    return new Promise ( async(resolve,reject)=>{
+        dataSet().forEach((dataPoint,i)=>{
+            let distances=[]
+            for(let j=0;j<k;j++){
+                const clusterCenterX=chart.data.datasets[0].data[j].x
+                const clusterCenterY =chart.data.datasets[0].data[j].y
+                distances.push(Math.sqrt(((clusterCenterX-dataPoint.Age)**2)+((clusterCenterY-dataPoint.Body_mass_index)**2)))
+            }
+            const minValue=Math.min.apply(Math,distances)
+            const index =distances.indexOf(minValue)
+            chart.data.datasets[0].pointBackgroundColor[i+k]=chart.data.datasets[0].pointBackgroundColor[index]
+        })
+        chart.update(2000)
+        await new Promise(resolve =>setTimeout(resolve,2000))
+        resolve()
+    })
+    
+}
